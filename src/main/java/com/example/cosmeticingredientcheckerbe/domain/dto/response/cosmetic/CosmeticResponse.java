169b98082f0792994682326c084886b915daf06b
HungPD <hungpd170501@gmail.com>
2023-05-19 01:56:15 +0700

    update CosmeticResponse with Long data type for id

--- src/main/java/com/example/cosmeticingredientcheckerbe/domain/dto/response/cosmetic/CosmeticResponse.java
+++ src/main/java/com/example/cosmeticingredientcheckerbe/domain/dto/response/cosmetic/CosmeticResponse.java
@@ -6,7 +6,7 @@ import lombok.Setter;
 @Getter
 @Setter
 public class CosmeticResponse {
-    private int productId;
+    private Long productId;
     private String name;
     private String description;
     private String image;
